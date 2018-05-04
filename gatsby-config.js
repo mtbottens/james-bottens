ed562ea4e09e473694188be79af72310a1defb6d
Michael Bottens <mbottens@gorillagroup.com>
2018-05-04 12:31:08 -0400

    derp

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        trackingId: "UA-118636235-1",
         head: false,
         anonymize: true,
         respectDNT: true,
