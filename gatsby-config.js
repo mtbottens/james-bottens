613d9281987f82d57c1c0e0bd5369f88df03b004
Michael Bottens <mbottens@gorillagroup.com>
2018-05-04 11:53:08 -0400

    update conf

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Strata by HTML5 UP",
-    author: "Hunter Chang",
-    description: "A Gatsby.js Starter based on Strata by HTML5 UP"
+    title: "James Bottens",
+    author: "Michael Bottens",
+    description: "Remembering James Bottens with fun photos."
   },
   pathPrefix: '/',
   plugins: [
